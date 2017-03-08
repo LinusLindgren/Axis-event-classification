8fc35900c675bfd7a56daeaf12e58c13338666af
Mange <dat12ml1@student.lth.se>
2017-03-08 15:27:09 +0100

    added pauto and autobin and optimized params

--- Matlab/exjobb/Matlab ACC/calc_dft.m
+++ Matlab/exjobb/Matlab ACC/calc_dft.m
@@ -1,6 +1,6 @@
-function [dft_samples] = calc_dft( samples,nposfiles,nnegfiles)
+function [dft_samples] = calc_dft( samples,nposfiles,nnegfiles,nbrOfSamples)
  
-dft_samples = zeros(256,3,size(samples,3));
+dft_samples = zeros(nbrOfSamples,3,size(samples,3));
 for i = 1 : size(samples,3)
  dft_samples(:,1,i) = fft(samples(:,1,i)); 
  dft_samples(:,2,i) = fft(samples(:,2,i));  
@@ -16,38 +16,38 @@ end
 % 
 % 
 % 
-% P2 = abs(dft_samples(:,1,random_pos_file)/256);
-% P1 = P2(1:256/2+1);
+% P2 = abs(dft_samples(:,1,random_pos_file)/nbrOfSamples);
+% P1 = P2(1:nbrOfSamples/2+1);
 % P1(2:end-1) = 2*P1(2:end-1);
-% f = 200*(0:(256/2))/256;
+% f = 200*(0:(nbrOfSamples/2))/nbrOfSamples;
 % plot(f,P1, 'g')
 % title('Single-Sided Amplitude Spectrum of X(t)')
 % xlabel('f (Hz)')
 % ylabel('|P1(f)|')
 % hold on;
-% P2 = abs(dft_samples(:,1,random_neg_file)/256);
-% P1 = P2(1:256/2+1);
+% P2 = abs(dft_samples(:,1,random_neg_file)/nbrOfSamples);
+% P1 = P2(1:nbrOfSamples/2+1);
 % P1(2:end-1) = 2*P1(2:end-1);
-% f = 200*(0:(256/2))/256;
+% f = 200*(0:(nbrOfSamples/2))/nbrOfSamples;
 % plot(f,P1, 'r')
 % title('Single-Sided Amplitude Spectrum of X(t)')
 % xlabel('f (Hz)')
 % ylabel('|P1(f)|')
 % 
 % figure;
-% P2 = abs(dft_samples(:,2,random_pos_file)/256);
-% P1 = P2(1:256/2+1);
+% P2 = abs(dft_samples(:,2,random_pos_file)/nbrOfSamples);
+% P1 = P2(1:nbrOfSamples/2+1);
 % P1(2:end-1) = 2*P1(2:end-1);
-% f = 200*(0:(256/2))/256;
+% f = 200*(0:(nbrOfSamples/2))/nbrOfSamples;
 % plot(f,P1, 'g')
 % title('Single-Sided Amplitude Spectrum of X(t)')
 % xlabel('f (Hz)')
 % ylabel('|P1(f)|')
 % hold on;
-% P2 = abs(dft_samples(:,2,random_neg_file)/256);
-% P1 = P2(1:256/2+1);
+% P2 = abs(dft_samples(:,2,random_neg_file)/nbrOfSamples);
+% P1 = P2(1:nbrOfSamples/2+1);
 % P1(2:end-1) = 2*P1(2:end-1);
-% f = 200*(0:(256/2))/256;
+% f = 200*(0:(nbrOfSamples/2))/nbrOfSamples;
 % plot(f,P1, 'r')
 % title('Single-Sided Amplitude Spectrum of X(t)')
 % xlabel('f (Hz)')
@@ -55,19 +55,19 @@ end
 % 
 % figure;
 % 
-% P2 = abs(dft_samples(:,3,random_pos_file)/256);
-% P1 = P2(1:256/2+1);
+% P2 = abs(dft_samples(:,3,random_pos_file)/nbrOfSamples);
+% P1 = P2(1:nbrOfSamples/2+1);
 % P1(2:end-1) = 2*P1(2:end-1);
-% f = 200*(0:(256/2))/256;
+% f = 200*(0:(nbrOfSamples/2))/nbrOfSamples;
 % plot(f,P1, 'g')
 % title('Single-Sided Amplitude Spectrum of X(t)')
 % xlabel('f (Hz)')
 % ylabel('|P1(f)|')
 % hold on;
-% P2 = abs(dft_samples(:,3,random_neg_file)/256);
-% P1 = P2(1:256/2+1);
+% P2 = abs(dft_samples(:,3,random_neg_file)/nbrOfSamples);
+% P1 = P2(1:nbrOfSamples/2+1);
 % P1(2:end-1) = 2*P1(2:end-1);
-% f = 200*(0:(256/2))/256;
+% f = 200*(0:(nbrOfSamples/2))/nbrOfSamples;
 % plot(f,P1, 'r')
 % title('Single-Sided Amplitude Spectrum of X(t)')
 % xlabel('f (Hz)')
