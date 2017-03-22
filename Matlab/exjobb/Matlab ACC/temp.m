4f15b2b01e1647a210b5377c7b74cfffb70e8a44
Mange <dat12ml1@student.lth.se>
2017-03-22 11:53:43 +0100

    added script for plotting ratio for different sampling time and frequencies

--- Matlab/exjobb/Matlab ACC/temp.m
+++ Matlab/exjobb/Matlab ACC/temp.m
@@ -1,9 +1,17 @@
 %% read samples
 clear, clc, close all
-%
-nbrOfSamples = 256;
 
-target_freq = 12.5;
+res = zeros(6, 5);
+l = 2;
+for freq=1 : 5 
+    k = 1;
+    l = l /2;
+    for samp_time =1 : 3
+%
+nbrOfSamples = 256 *k
+k = k /2;
+target_freq = 200 * l
+
 [nposfiles1,nnegfiles1,samples1] = parse_acc_files(nbrOfSamples,'acc_data\postemp\acc*' ...
 , 'acc_data\negtemp\acc*');
 
@@ -72,54 +80,19 @@ der_min = squeeze(min(derivate,[],1));
 der_sum = squeeze(sum(derivate,1));
 moments = squeeze(moment(samples,3));
 
-%% compute dft values
-close all;
-dft_samples = calc_dft(samples,nposfiles,nnegfiles,nbrOfSamples);
-%fix tiltVector
-tiltAsSamp = zeros(nbrOfSamples,3,nposfiles+nnegfiles);
-tiltAsSamp(:,1,:) = tiltXZ;
-tiltAsSamp(:,2,:) = tiltYZ;
-tiltAsSamp(:,3,:) = tiltXY;
-dft_tilt = calc_dft(tiltAsSamp,nposfiles,nnegfiles,nbrOfSamples);
-
-[sortedValuesSamplesX,sortIndexSamplesX] = sort(dft_samples(:,1,:),'descend');
-[sortedValuesSamplesY,sortIndexSamplesY] = sort(dft_samples(:,2,:),'descend');
-[sortedValuesSamplesZ,sortIndexSamplesZ] = sort(dft_samples(:,3,:),'descend');
-%dft_samples_k_max_freq = zeros(5*3*(nposfiles+nnegfiles),1);
-max_k_freq = 5;
-dft_samples_k_max_freq = zeros(max_k_freq,nposfiles+nnegfiles);
-dft_samples_k_max_val = zeros(max_k_freq,nposfiles+nnegfiles);
-
-
-[sortedValuesTiltX,sortIndexTiltX] = sort(dft_tilt(:,1,:),'descend');
-[sortedValuesTiltY,sortIndexTiltY] = sort(dft_tilt(:,2,:),'descend');
-[sortedValuesTiltZ,sortIndexTiltZ] = sort(dft_tilt(:,3,:),'descend');
-dft_tilt_k_max_freq = zeros(max_k_freq,nposfiles+nnegfiles);
-dft_tilt_k_max_val = zeros(max_k_freq,nposfiles+nnegfiles);
-
-for i = 1 : nposfiles+nnegfiles
-        %startIndex = (i-1)*15+1
-        for k = 1 : max_k_freq
-    dft_samples_k_max_freq(k,i) = squeeze(sortIndexSamplesX(k,1,i));
-    dft_samples_k_max_freq(k,i) = squeeze(sortIndexSamplesY(k,1,i));
-    dft_samples_k_max_freq(k,i) = squeeze(sortIndexSamplesZ(k,1,i));
-    dft_samples_k_max_val(k,i) = squeeze(sortedValuesSamplesX(k,1,i));
-    dft_samples_k_max_val(k,i) = squeeze(sortedValuesSamplesY(k,1,i));
-    dft_samples_k_max_val(k,i) = squeeze(sortedValuesSamplesZ(k,1,i));
-    dft_tilt_k_max_freq(k,i) = squeeze(sortIndexTiltX(k,1,i));
-    dft_tilt_k_max_freq(k,i) = squeeze(sortIndexTiltY(k,1,i));
-    dft_tilt_k_max_freq(k,i) = squeeze(sortIndexTiltZ(k,1,i));
-    dft_tilt_k_max_val(k,i) = squeeze(sortedValuesTiltX(k,1,i));
-    dft_tilt_k_max_val(k,i) = squeeze(sortedValuesTiltY(k,1,i));
-    dft_tilt_k_max_val(k,i) = squeeze(sortedValuesTiltZ(k,1,i));
-        end
-end
-
-
 
 %% Extract correlation features
 
-lag = 7;
+if(nbrOfSamples > 64)
+    lag = 30;
+elseif(nbrOfSamples > 20)
+    lag = 20;
+elseif(nbrOfSamples > 7)
+    lag = 7;
+else
+    lag = 3;
+end
+
 [cross_corr_max, sum_auto, min_auto, sum_pauto, min_pauto, auto_bins, auto_corr_flat, auto_corr] = extract_corr_features(samples,nposfiles,nnegfiles,lag);
 skewness_acor_samples = squeeze(skewness(auto_corr))';
 kurtosis_acor_samples = squeeze(kurtosis(auto_corr))';
@@ -145,31 +118,46 @@ der_sum_auto_corr = squeeze(sum(derivate_auto_corr,1));
 
 
 %% perform training and testing
-clc
 write_svm_model_to_file = 0;
-attempts = 1000;
+attempts = 100;
 alpha = 0.75;
 
 
-[averageTestRatio, averageTrainRatio, true_positive, false_positive, countMissclassifications,SVMModel] ...
+[ratio, averageTrainRatio, res((samp_time-1)*2+1,freq), res((samp_time-1)*2+2,freq), countMissclassifications,SVMModel] ...
 = train_and_test_scm_model(attempts, alpha, nposfiles,nnegfiles, sum_auto, min_auto, cross_corr_max,auto_corr_flat ...
 ,auto_bins,meanFeatures, meanTiltFeatures, stdFeatures, stdTiltFeatures,sumFeatures,minFeatures,maxFeatures, ...
 maxTiltFeatures,minTiltFeatures, skewness_samples, kurtosis_samples, sum_changes, mean_changes, ...
 der_min, der_max, der_mean, der_sum ,sumAbsFeatures,sumAllDimFeatures, moments, skewness_acor_samples, kurtosis_acor_samples, ...
 sum_changes_auto, mean_changes_auto, der_min_auto_corr, der_max_auto_corr, der_mean_auto_corr, der_sum_auto_corr,write_svm_model_to_file);
-%% plot feature clustering
-plot_feature_clustering(sum_changes(2,:), min_auto(:,3)',nposfiles,nnegfiles);
+   ratio
+    end
+end
+%% plot 
+close all
+%128 = 1.28, it is the time of the sampling
+ft_128 = fit(x',res(1,:)','smoothingspline');
+ff_128 = fit(x',res(2,:)','smoothingspline');
+figure
+plot(ft_128,'b',x',res(1,:)','.b');
+hold on
+plot(ff_128,'r',x',res(2,:)','.r');
+title('Ratio for 1.28 (s) sampling time for different frequencies');
+legend('Data true positive','true positive','data false positive', 'false positive');
 
+ft_64 = fit(x',res(3,:)','smoothingspline');
+ff_64 = fit(x',res(4,:)','smoothingspline');
+figure
+plot(ft_64,'b',x',res(3,:)','.b');
+hold on
+plot(ff_64,'r',x',res(4,:)','.r');
+title('Ratio for 0.64 (s) sampling time for different frequencies');
+legend('Data true positive','true positive','data false positive', 'false positive');
 
-%% plot train and testing ratio over alpha
-max_alpha = 1.00;
-min_alpha = 0.05;
-write_svm_model_to_file = 0;
-[training_precision, testing_precision] = plot_ratio_over_alpha(min_alpha, max_alpha, attempts, cross_corr_max, sum_auto, min_auto, sum_pauto, min_pauto, auto_bins, auto_corr_flat, nposfiles, nnegfiles, write_svm_model_to_file);
-
-%% Plot decrease
-max_samples = 256;
-min_samples = 64;
-nbr_steps = 32;
-write_svm_model_to_file = 0;
-[max_true_positive,min_false_positive ,lag_index_true_positive, lag_index_false_positive , corresponding_false_positive, corresponding_true_positive] = plot_decrease_sample_size(samples, max_samples,min_samples, nbr_steps,nposfiles,nnegfiles,lag, attempts,alpha, write_svm_model_to_file);
+ft_32 = fit(x',res(5,:)','smoothingspline');
+ff_32 = fit(x',res(6,:)','smoothingspline');
+figure
+plot(ft_32,'b',x',res(5,:)','.b');
+hold on
+plot(ff_32,'r',x',res(6,:)','.r');
+title('Ratio for 0.32 (s) sampling time for different frequencies');
+legend('Data true positive','true positive','data false positive', 'false positive');
